ba23b4845ff1829796e229743c646a8aa706ef7f
mordy <imky171@gmail.com>
2022-08-09 12:49:09 -0400

    Formatted with black ✨

--- main.py
+++ main.py
@@ -15,40 +15,43 @@ env = {
 	"bk": lambda x: bob.bk(float(x)),
 	"rt": lambda x: bob.rt(float(x)),
 	"lt": lambda x: bob.lt(float(x)),
-	'square': lambda x: square(float(x)),
-	'circle': lambda x: circle(float(x)),
-	'color': lambda x: color(x),
-	'hide': lambda: bob.ht(),
-	'show': lambda: bob.st(),
-	'begin-fill': lambda: bob.begin_fill(),
-	'end-fill': lambda: bob.end_fill(),
-	'bgcolor': lambda x: screen.bgcolor(x),
+	"square": lambda x: square(float(x)),
+	"circle": lambda x: circle(float(x)),
+	"color": lambda x: color(x),
+	"hide": lambda: bob.ht(),
+	"show": lambda: bob.st(),
+	"begin-fill": lambda: bob.begin_fill(),
+	"end-fill": lambda: bob.end_fill(),
+	"bgcolor": lambda x: screen.bgcolor(x),
 }
+
 
 def execute_command(command, arg):
 	env.get(command)(arg)
 
+
 def set_var(env: dict, args):
 	env.update({args[0]: args[1]})
+
 
 def run_code():
 	bob.reset()
 	lines = code.get("1.0", END).strip().split("\n")
 	for line in lines:
 		func = line.split()
-		if len(func) == 0 or func[0].startswith('#'):
+		if len(func) == 0 or func[0].startswith("#"):
 			continue
-		elif line.startswith('!py '):
-			line = line.split(' ')
+		elif line.startswith("!py "):
+			line = line.split(" ")
 			line.pop(0)
-			line = ' '.join(line)
+			line = " ".join(line)
 			exec(line, globals(), locals())
 			continue
 		elif func[0] == "loop":
-			times = func[1].strip('times')
+			times = func[1].strip("times")
 			times = env.get(times, times)
-			commands = ' '.join(func[2:])
-			commands = commands.split(';')
+			commands = " ".join(func[2:])
+			commands = commands.split(";")
 			for idx, _ in enumerate(commands):
 				commands[idx] = commands[idx].strip().split()
 			for _ in range(int(times)):
@@ -76,13 +79,16 @@ def run_code():
 						f"It appears that an argument is missing in {func[0]!r}",
 					)
 
+
 def square(size):
 	for i in range(4):
 		bob.fd(size)
 		bob.rt(90)
 
+
 def circle(size):
 	bob.circle(size)
+
 
 def color(color_str):
 	bob.color(color_str)
@@ -91,7 +97,7 @@ def color(color_str):
 root = tk.Tk()
 root.title("Play Turtle")
 root.resizable(False, False)
-root.iconphoto(True, tk.PhotoImage(file='turtle.png'))
+root.iconphoto(True, tk.PhotoImage(file="turtle.png"))
 
 canvas = tk.Canvas(root)
 canvas.config(width=500, height=500)
